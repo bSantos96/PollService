c421ece281e35bccf7166d491f249054616d525f
bSantos96 <87330842+bSantos96@users.noreply.github.com>
2023-02-12 18:55:16 +0000

    PollService.Database (#1)
    Create a console application to facilitate database migrations using the dbUp nuget package.

---
+++ PollService.Database/PollService.Database/Extensions/AppConfigExtensions.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="AppSettingsExtensions.cs" company="Bruno Santos">
+// Copyright (c) Bruno Santos. All rights reserved.
+// </copyright>
+
+namespace PollService.Database.Extensions
+{
+    using Microsoft.Extensions.Configuration;
+
+    /// <summary>
+    /// Extension class to perform operation on App_Config files.
+    /// </summary>
+    public static class AppConfigExtensions
+    {
+        /// <summary>
+        /// Gets settings.
+        /// </summary>
+        /// <typeparam name="T">The settings type.</typeparam>
+        /// <param name="appSettingsFile">The application settings file.</param>
+        /// <returns>The mapped settings.json file.</returns>
+        public static T GetSettings<T>(string appSettingsFile)
+            where T : class
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "App_Config"))
+                .AddJsonFile(appSettingsFile, optional: false)
+                .Build();
+
+            return configuration.Get<T>();
+        }
+    }
+}
