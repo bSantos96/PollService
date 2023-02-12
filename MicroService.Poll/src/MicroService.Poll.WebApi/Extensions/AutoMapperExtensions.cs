97cb2415a4b05cb8cdff763dfd08d9c04abfb008
Bruno Santos <bruno.lgac.santos@gmail.com>
2023-02-12 20:11:36 +0000

    MicroService.Poll soluction setup

---
+++ MicroService.Poll/src/MicroService.Poll.WebApi/Extensions/AutoMapperExtensions.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="AutoMapperExtensions.cs" company="Bruno Santos">
+// Copyright (c) Bruno Santos. All rights reserved.
+// </copyright>
+
+namespace MicroService.Poll.WebApi.Extensions
+{
+    using System.Reflection;
+
+    /// <summary>
+    /// <see cref="AutoMapper"/> extensions for dependency injection.
+    /// </summary>
+    public static class AutoMapperExtensions
+    {
+        /// <summary>
+        /// Configures dependency injection with <see cref="AutoMapper"/>.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <param name="assemblies">The scanned assemblies.</param>
+        /// <returns>The <see cref="IServiceCollection"/> configured.</returns>
+        public static IServiceCollection AddCustomAutoMapper(this IServiceCollection services, IEnumerable<Assembly> assemblies)
+        {
+            return services.AddAutoMapper(assemblies);
+        }
+    }
+}
