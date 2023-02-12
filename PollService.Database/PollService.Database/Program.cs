e5ba803b9cdf70d90ccee86f9decf119ea0b5e48
Bruno Santos <bruno.lgac.santos@gmail.com>
2023-02-12 18:53:43 +0000

    Create a console application to facilitate database migrations using the dbUp nuget package.

--- PollService.Database/PollService.Database/Program.cs
+++ PollService.Database/PollService.Database/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// <copyright file="Program.cs" company="Bruno Santos">
+// Copyright (c) Bruno Santos. All rights reserved.
+// </copyright>
+
+namespace PollService.Database
+{
+    using DbUp;
+    using DbUp.Engine;
+    using PollService.Database.Extensions;
+    using PollService.Database.Options;
+    using System.Reflection;
+
+    /// <summary>
+    /// The program class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main method.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns>Result of app execution.</returns>
+        static int Main(string[] args)
+        {
+            var result = UpgradeDatabase();
+            return ProcessResult(result);
+        }
+        
+        private static DatabaseUpgradeResult UpgradeDatabase()
+        {
+            var connectionString = AppConfigExtensions
+                .GetSettings<DatabaseSettings>("Database.settings.json")
+                .ConnectionString;
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            return upgrader.PerformUpgrade();
+        }
+
+        private static int ProcessResult(DatabaseUpgradeResult result)
+        {
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success");
+            Console.ResetColor();
+
+            return 0;
+        }
+    }
+}
---
+++ PollService.Database/PollService.Database/Scripts/0001-Questions/03.TR_Question_LastUpdateDateUtc.ddl.sql
@@ -0,0 +1,10 @@
+﻿CREATE OR ALTER TRIGGER [dbo].[TR_Question_LastUpdateDateUtc]
+ON [dbo].[Question]
+AFTER UPDATE
+AS
+BEGIN
+	UPDATE [Question]
+	SET [LastUpdateDateUtc] = SYSDATETIMEOFFSET()
+	FROM inserted
+	WHERE [Question].[QuestionId] = inserted.[QuestionId]
+END
